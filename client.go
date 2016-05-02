6d60fe50b5e1625af169e76ba043b044c571f8a8
Eric Waters <waters@google.com>
2016-05-02 17:22:20 -0400

    Client

---
+++ client.go
@@ -0,0 +1,144 @@
+package freeling
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net"
+)
+
+const (
+	flServerReady = "FL-SERVER-READY"
+)
+
+type Client struct {
+	conn net.Conn
+	br   *bufio.Reader
+}
+
+func New(addr string) (*Client, error) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("net.Dial(_, %q) failed: %v", addr, err)
+	}
+	c := &Client{
+		conn: conn,
+		br:   bufio.NewReader(conn),
+	}
+
+	if _, err = c.Send("RESET_STATS"); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+type Token struct {
+	CTag   string
+	Case   string
+	Form   string
+	Gen    string
+	ID     string
+	Lemma  string
+	Mood   string
+	Num    string
+	POS    string
+	Person string
+	Tag    string
+	Tense  string
+	Type   string
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("%s %s %s", t.Form, t.Lemma, t.Tag)
+}
+
+type Phrase struct {
+	ID     string
+	Tokens []Token
+}
+
+func (p Phrase) String() string {
+	var b bytes.Buffer
+	fmt.Fprintf(&b, "Phrase %s:\n", p.ID)
+	for _, t := range p.Tokens {
+		fmt.Fprintf(&b, "  %s\n", t)
+	}
+	return b.String()
+}
+
+type Document struct {
+	Phrases []Phrase
+}
+
+func (d Document) String() string {
+	var b bytes.Buffer
+	for _, p := range d.Phrases {
+		b.WriteString(p.String())
+	}
+	return b.String()
+}
+
+func (c *Client) Process(msg string) (*Document, error) {
+	s, err := c.Send(msg)
+	if err != nil {
+		return nil, err
+	}
+	d := &Document{
+		Phrases: make([]Phrase, 0),
+	}
+	if err := json.Unmarshal([]byte("["+s+"]"), &d.Phrases); err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+func (c *Client) Send(msg string) (string, error) {
+	if err := c.write(msg); err != nil {
+		return "", err
+	}
+
+	ret := ""
+
+	res, err := c.read()
+	if err != nil {
+		return "", err
+	}
+	if res != flServerReady {
+		ret = res
+	}
+
+	if err := c.write("FLUSH_BUFFER"); err != nil {
+		return "", err
+	}
+
+	res, err = c.read()
+	if err != nil {
+		return "", err
+	}
+	if res != flServerReady {
+		ret += res
+	}
+	return ret, nil
+}
+
+func (c *Client) write(msg string) error {
+	// log.Printf(">>> %q", msg)
+	_, err := fmt.Fprint(c.conn, msg+"\u0000")
+	return err
+}
+
+func (c *Client) read() (string, error) {
+	b, err := c.br.ReadBytes(0)
+	if err != nil {
+		return "", err
+	}
+	s := string(b[:len(b)-1])
+	// log.Printf("<<< %q", s)
+	return s, err
+}
