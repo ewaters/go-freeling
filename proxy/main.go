d0b2548fe73bd941ae4d647c8d2681bb1f543b81
Eric Waters <ewaters@shutterstock.com>
2017-11-26 12:45:19 -0600

    Update Dockerfile; support multiple languages

--- proxy/main.go
+++ proxy/main.go
@@ -12,9 +12,11 @@ import (
 )
 
 var (
-	flAddr = flag.String("fl_addr", "", "")
 	addr   = flag.String("addr", ":8080", "")
 	debug  = flag.Bool("debug", false, "")
+	langs = flag.String("langs", "de=:10001;es=:10002", "")
+
+	clients map[string]*freeling.Client
 )
 
 func logExit(msg string, args ...interface{}) {
@@ -22,27 +24,54 @@ func logExit(msg string, args ...interface{}) {
 	os.Exit(1)
 }
 
+
 func main() {
 	flag.Parse()
 
-	if *flAddr == "" {
-		logExit("--fl_addr=... is required")
+	if *langs == "" {
+		logExit("--langs=... is required")
+	}
+	config := map[string]string{}
+	for _, pair := range strings.Split(*langs, ";") {
+		parts := strings.SplitN(pair, "=", 2)
+		if len(parts) != 2 {
+			logExit("--config %q was invalid; must take form '<lang>=<addr>[;...]'", *langs) 
+		}
+		config[parts[0]] = parts[1]
 	}
 
-	client, err := freeling.New(*flAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client.Debug = *debug
-	defer client.Close()
 
-	http.HandleFunc("/freeling-es-json", func(w http.ResponseWriter, r *http.Request) {
+	clients = make(map[string]*freeling.Client)
+	for lang, addr := range config {
+		client, err := freeling.New(addr)
+		if err != nil {
+			logExit("Failed to create Freeling client for %s (%q): %v", lang, addr, err)
+		}
+		client.Debug = *debug
+		defer client.Close()
+		clients[lang] = client
+	}
+
+	for lang := range clients {
+		http.HandleFunc(fmt.Sprintf("/freeling-%s-json", lang), handlerForLang(lang))
+	}
+	
+	log.Printf("Listening for HTTP connections on %s...", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+func handlerForLang(lang string) func(http.ResponseWriter, *http.Request) {
+	client := clients[lang]
+	if client == nil {
+		logExit("No client found for language %s", lang)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
 		failed := func(format string, args ...interface{}) {
 			fmt.Fprintf(w, "Failed: "+format, args...)
 		}
 
 		msg := r.FormValue("text")
-		log.Printf("Request from %s for %q", r.RemoteAddr, msg)
+		log.Printf("[%s] Request from %s for %q", lang, r.RemoteAddr, msg)
 		if msg == "" {
 			failed("'text' required")
 			return
@@ -60,8 +89,5 @@ func main() {
 			fmt.Printf("\n\n[%s]\n\n", res)
 		}
 		fmt.Fprintf(w, "[%s]", res)
-	})
-
-	log.Printf("Listening for HTTP connections on %s...", *addr)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	}
 }
